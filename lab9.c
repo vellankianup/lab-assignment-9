1d31eafb8e248077a41dd621a1d3907266edbc34
Anup Vellanki <vellankianup@gmail.com>
2023-04-14 22:19:06 -0400

    code added

--- lab9.c
+++ lab9.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 // RecordType
 struct RecordType
@@ -11,12 +13,15 @@ struct RecordType
 // Fill out this structure
 struct HashType
 {
+	struct RecordType record;
+    int isOccupied;
 
 };
 
 // Compute the hash function
 int hash(int x)
 {
+	return x % 100;
 
 }
 
@@ -75,20 +80,91 @@ void printRecords(struct RecordType pData[], int dataSz)
 // index x -> id, name, order -> id, name, order ....
 void displayRecordsInHash(struct HashType *pHashArray, int hashSz)
 {
-	int i;
+	int i, j;
+    struct RecordType record;
 
-	for (i=0;i<hashSz;++i)
-	{
-		// if index is occupied with any records, print all
-	}
+    for (i = 0; i < 11; ++i)
+    {
+        if (pHashArray[i].isOccupied)
+        {
+            printf("Index %d ->", i);
+
+            record = pHashArray[i].record;
+            printf(" %d, %c, %d", record.id, record.name, record.order);
+
+            j = i + 1;
+            while (j < hashSz && pHashArray[j].isOccupied && hash(pHashArray[j].record.id) == i)
+            {
+                record = pHashArray[j].record;
+                printf(" -> %d, %c, %d", record.id, record.name, record.order);
+                j++;
+            }
+            printf(" -> NULL\n");
+        }
+        else
+        {
+            printf("Index %d -> NULL\n", i);
+        }
+    }
 }
 
 int main(void)
-{
-	struct RecordType *pRecords;
-	int recordSz = 0;
 
-	recordSz = parseData("input.txt", &pRecords);
-	printRecords(pRecords, recordSz);
-	// Your hash implementation
+{
+    struct RecordType *pRecords;
+    int recordSz = 0;
+
+    recordSz = parseData("input.txt", &pRecords);
+    printRecords(pRecords, recordSz);
+
+    // Initialize the hash array
+    int hashSz = recordSz;
+    struct HashType *pHashArray = (struct HashType*) malloc(sizeof(struct HashType) * 11);
+    if (pHashArray == NULL)
+    {
+        printf("Cannot allocate memory for hash array\n");
+        exit(-1);
+    }
+     memset(pHashArray, 0, sizeof(struct HashType) * hashSz);
+
+    // Insert records into the hash array
+    int i;
+    for (i = 0; i < recordSz; i++)
+    {
+        int idx = hash(pRecords[i].id) % hashSz;
+        struct HashType *pHash = pHashArray + idx;
+        if (pHash->isOccupied == 0)
+        {
+            
+            pHash->record.id = pRecords[i].id;
+            pHash->record.name = pRecords[i].name;
+            pHash->record.order = pRecords[i].order;
+            pHash->isOccupied = 1;
+        }
+        else
+        {
+            
+            while (pHash->isOccupied != 0)
+            {
+                ++idx;
+                if (idx == hashSz)
+                {
+                    idx = 0;
+                }
+                pHash = pHashArray + idx;
+            }
+            pHash->record.id = pRecords[i].id;
+            pHash->record.name = pRecords[i].name;
+            pHash->record.order = pRecords[i].order;
+            pHash->isOccupied = 1;
+        }
+    }
+
+    // Display records in the hash array
+    displayRecordsInHash(pHashArray, hashSz);
+
+    free(pHashArray);
+    free(pRecords);
+
+    return 0;
 }
